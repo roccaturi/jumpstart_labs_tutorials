5ee7e37b2cd7e49eadf8a2fcba68799727aae728
roccaturi <brian.burnley@gmail.com>
2015-06-16 14:10:53 -0400

    Tagging feature implemented

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    tags.join(', ')
+  end
+
+  def tag_list=(tags_string)
+    tags = tags_string.split(',').collect {|t| t.downcase.strip}.uniq
+    new_and_existing_tags = tags.collect { |t| Tag.find_or_create_by(name: t) }
+    self.tags = new_and_existing_tags
+  end
 end
